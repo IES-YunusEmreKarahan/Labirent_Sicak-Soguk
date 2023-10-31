0fcbe77fb1cae521e7c75687879023ab6fee79f9
IES-YunusEmreKarahan <147061126+IES-YunusEmreKarahan@users.noreply.github.com>
2023-10-31 08:42:27 +0000

    Projede bulunan hatalar düzeltildi..

---
+++ MyMazeLibrary/MyMazeProgram/Algorithm.cs
@@ -0,0 +1,112 @@
+﻿class Algorithm
+{
+
+    public static List<Node> AStar(string[,] Maze, Position start, Position end)
+    {
+        int rows = Maze.GetLength(0);
+        int cols = Maze.GetLength(1);
+
+        int[] dx = { -1, 0, 1, 0 };
+        int[] dy = { 0, 1, 0, -1 };
+
+        var openList = new List<Node>();
+        var closedList = new List<Node>();
+
+        openList.Add(new Node(start, null, 0, CalculateHeuristic(start, end)));
+
+        while (openList.Any())
+        {
+            var current = openList.OrderBy(node => node.Cost).First();
+
+            if (current.Position.Equals(end))
+            {
+                return ReconstructPath(current);
+            }
+
+            openList.Remove(current);
+            closedList.Add(current);
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newRow = current.Position.Row + dx[i];
+                int newCol = current.Position.Col + dy[i];
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols
+                    && Maze[newRow, newCol] != "■"
+                    && !closedList.Any(node => node.Position.Row == newRow && node.Position.Col == newCol))
+                {
+                    var neighbor = new Position(newRow, newCol);
+                    var g = current.G + 1;
+                    var h = CalculateHeuristic(neighbor, end);
+
+                    var existingNode = openList.FirstOrDefault(node => node.Position.Row == newRow && node.Position.Col == newCol);
+
+                    if (existingNode == null)
+                    {
+                        openList.Add(new Node(neighbor, current, g, h));
+                    }
+                    else if (g + h < existingNode.Cost)
+                    {
+                        existingNode.Parent = current;
+                        existingNode.G = g;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    static List<Node> ReconstructPath(Node node)
+    {
+        var path = new List<Node>();
+        while (node != null)
+        {
+            path.Add(node);
+            node = node.Parent;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    static int CalculateHeuristic(Position from, Position to)
+    {
+        // Manhattan mesafesi hesaplama
+        return Math.Abs(from.Row - to.Row) + Math.Abs(from.Col - to.Col);
+    }
+
+}
+
+class Node
+{
+    public Position Position { get; }
+    public Node Parent { get; set; }
+    public int G { get; set; }
+    public int H { get; }
+    public int Cost => G + H;
+
+    public Node(Position position, Node parent, int g, int h)
+    {
+        Position = position;
+        Parent = parent;
+        G = g;
+        H = h;
+    }
+}
+
+class Position
+{
+    public int Row { get; }
+    public int Col { get; }
+
+    public Position(int row, int col)
+    {
+        Row = row;
+        Col = col;
+    }
+
+    public bool Equals(Position other)
+    {
+        return Row == other.Row && Col == other.Col;
+    }
+}
---
+++ MyMazeLibrary/MyMazeProgram/Clue.cs
@@ -0,0 +1,101 @@
+﻿using MyMazeLibrary;
+using System.Drawing;
+using static MyMazeLibrary.MazeFunctions;
+using Console = Colorful.Console; // Renk Eklentisi
+using Colorful; // Renk Eklentisi
+using System.Collections.Generic;
+class Clue
+{
+    public static void ClueDetector(ConsoleKeyInfo key, int shortestPathLength, int exitDistanceLength, List<Node> exitDistance)
+    {
+        if (key.KeyChar == 'i' || key.KeyChar == 'İ') // İpucu fonksiyonu
+        {
+            MazeData.showclue = true;
+
+            PrintClue(shortestPathLength, exitDistanceLength, exitDistance);
+
+        }
+        else if (MazeData.showclue == true && MazeData.MazeVisible == false)
+        {
+            PrintClue(shortestPathLength, exitDistanceLength, exitDistance);
+        }
+    }
+
+    public static void PrintClue(int shortestPathLength, int exitDistanceLength, List<Node> exitDistance)
+    {
+        if (MazeData.MazeVisible == true) // Labirent ipucu
+        {
+
+            for (int i = 0; i < MazeData.matrixSize; i++)
+            {
+                for (int j = 0; j < MazeData.matrixSize; j++)
+                {
+                    if (MazeData.Maze[i, j] == "☻")
+                    {
+                        MazeData.Maze[i, j] = ".";
+                    }
+                }
+            }
+
+            int playerLine = MazeData.playerLine;
+            int playerColumn = MazeData.playerColumn;
+
+            List<Node> clue = Algorithm.AStar(MazeData.Maze, new Position(playerLine, playerColumn), new Position(MazeData.exitLine, MazeData.exitColumn));
+
+            if (clue != null)
+            {
+                foreach (Node node in clue)
+                {
+                    if (MazeData.Maze[playerLine, playerColumn] == ".")
+                    {
+                        MazeData.Maze[playerLine, playerColumn] = "☻";
+                    }
+
+                    playerLine = node.Position.Row;
+                    playerColumn = node.Position.Col;
+
+                    Console.Clear();
+                    bool BackgroundColorChange = false;
+                    PrintMaze(BackgroundColorChange, shortestPathLength, exitDistanceLength);
+
+                    Thread.Sleep(10);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bu Labirent çözülemez.");
+            }
+        }
+        else // Sıcak/Soğuk ipucu
+        {
+            if (exitDistance != null)
+            {
+                MazeData.ClueList = new List<string>();
+                for (int i = 1; i < exitDistance.Count; i++)
+                {
+                    int deltaX = exitDistance[i].Position.Col - exitDistance[i - 1].Position.Col;
+                    int deltaY = exitDistance[i].Position.Row - exitDistance[i - 1].Position.Row;
+
+                    if (deltaX == 1)
+                    {
+                        MazeData.ClueList.Add("Sağ");
+                    }
+                    else if (deltaX == -1)
+                    {
+                        MazeData.ClueList.Add("Sol");
+                    }
+                    else if (deltaY == 1)
+                    {
+                        MazeData.ClueList.Add("Aşağı");
+                    }
+                    else if (deltaY == -1)
+                    {
+                        MazeData.ClueList.Add("Yukarı");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
